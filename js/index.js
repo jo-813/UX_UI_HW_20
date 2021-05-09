a5ec6d8d5a2b7d6c2f046905b2d50d0b49a12112
jo-813 <72222605+jo-813@users.noreply.github.com>
2021-05-08 21:45:53 -0600

    HW20

---
+++ js/index.js
@@ -0,0 +1,4 @@
+console.log("Your index.js file is loaded correctly!");
+ScrollReveal().reveal('.content1 .img1');
+ScrollReveal().reveal('.content2', { delay: 1000 });
+ScrollReveal().reveal('.content3', { delay: 1300 });
